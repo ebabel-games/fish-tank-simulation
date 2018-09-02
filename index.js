414e325eb71b34882fe15d3daac7521c462f8705
Thomas Amar <hello@ebabel.eu>
2018-09-02 10:47:41 +0200

    switch order of endpoints

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 const fishes = [];
 
+
+
+app.get('/fishes', (req, res) => res.send('Your aquarium fish population: ' + fishes.length));
+
 app.get('/', (req, res) => res.send('Welcome to your virtual aquarium'));
-
-
-app.get('/fishes', (req, res) => res.send(`Your aquarium fish population: ${fishes.length}`));
-
 
 app.listen(3000, () => console.log('App listening on port 3000'));
 
