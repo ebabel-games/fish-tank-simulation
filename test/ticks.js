1ee0d0c2d4887da67e442287f077000c2976bb7b
Dave Pitts <dgapitts@gmail.com>
2018-09-09 11:36:05 +0100

    start ticks.js tests

---
+++ test/ticks.js
@@ -0,0 +1,44 @@
+const { describe, it } = require('mocha');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const assert = chai.assert;
+
+chai.use(chaiHttp);
+process.env.NODE_ENV = 'test';
+
+const Index = require('../src/index');
+const descriptions = require('../src/descriptions.json');
+
+describe('API ticks entity', () => {
+  it('should return a 200 http status, json type, an array of ticks, and expected body description', (done) => {
+    chai.request(Index)
+      .get('/ticks')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) console.error(err);   /* eslint no-console: 0 */
+        expect(res).to.have.status(200);
+        expect(res.type).to.eql('application/json');
+        assert(Array.isArray(res.body.ticks));
+        expect(res.body.description).to.eql(descriptions.ticks.get);
+        done();
+      });
+  });
+
+  it('should create a new tick, and get expected tick properties', (done) => {
+    chai.request(Index)
+      .put('/ticks')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) console.error(err);   /* eslint no-console: 0 */
+        expect(res).to.have.status(201);
+        expect(res.type).to.eql('application/json');
+        expect(res.body.description).to.eql(descriptions.ticks.put);
+        //expect(res.body.tick).not.to.eql(undefined);
+        //expect(res.body.tick.strength).not.to.eql(undefined);
+        //expect(res.body.tick.undefinedProperty).to.eql(undefined);
+        //assert(res.body.tick.defence > 0);
+        done();
+      });
+  });
+});
