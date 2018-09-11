bda72d4a7be0a0a094bb28f08814ca43aabe7d6a
Thomas Amar <hello@ebabel.eu>
2018-09-11 22:30:10 +0200

    develop basic version of the simulation client

--- game.js
+++ game.js
@@ -1,80 +1,90 @@
 const domain = 'https://fish-tank-simulation-uczfxcqaxz.now.sh';
 
-const callApi = (method = 'GET', url = domain) => {
+let latestLog;
+
+const updateLogs = (logs) => {
+  const logsWindow = document.getElementById('logsWindow');
+
+  logs.map((log) => {
+    const text = document.createTextNode(log);
+    const p = document.createElement('p');
+    p.appendChild(text);
+    logsWindow.appendChild(p);
+  });
+
+  // Scroll to the bottom of logsWindow.
+  logsWindow.scrollTop = logsWindow.scrollHeight;
+}
+
+const callApi = (method = 'GET', endpoint) => {
+  const url = `${domain}${endpoint}`;
   const xhr = new XMLHttpRequest();
   xhr.open(method, url, true);
   xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 201)) {
-      console.log(xhr.responseText);
+    if (xhr.readyState === 4 && xhr.status === 200 && endpoint === '/') {
+      const lastLog = JSON.parse(xhr.response).lastLog;
+      if (lastLog !== latestLog) {
+        latestLog = lastLog;
+        updateLogs([latestLog]);
+      }
     }
   };
-  xhr.send();  
+  xhr.send();
 };
 
-document.getElementById('controls').addEventListener('submit', (e) => {
-  e.preventDefault();
-});
-
-document.getElementById('putFishes').addEventListener('click', (e) => {
-  callApi('PUT', `${domain}/fishes`);
-});
-
-document.getElementById('putTicks').addEventListener('click', (e) => {
-  callApi('PUT', `${domain}/ticks`);
-});
-
-document.getElementById('getLogs').addEventListener('click', (e) => {
-  callApi('GET', `${domain}/logs`);
-});
-
-document.getElementById('getDataStore').addEventListener('click', (e) => {
-  callApi();
-});
-
+// THREE.js setup.
 const clock = new THREE.Clock();
-
-// Create an empty scene
 const scene = new THREE.Scene();
-
-// Create a basic perspective camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
-
-// Create a renderer with Antialiasing
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-// Configure renderer clear color
 renderer.setClearColor("#0e0727");
-
-// Configure renderer size
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Append Renderer to DOM
 document.body.appendChild(renderer.domElement);
 
-// Create a Cube Mesh with basic material
+// Create a Cube Mesh with basic material.
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
 const cube = new THREE.Mesh(geometry, material);
-
-// Add cube to Scene
 scene.add(cube);
 
-// Render Loop
+// Render Loop.
+let oneTick = 0;
+let twoTicks = 0;
+let sixTicks = 0;
 const render = () => {
-  // Calculate Delta.
-  const delta = clock.getDelta();
+  const delta = clock.getDelta(); // Calculate Delta.
 
-  const speedInUnitsPerSecond = 2;
+  // Cube animation.
+  const cubeSpeedInUnitsPerSecond = 2;
+  cube.rotation.x += delta * cubeSpeedInUnitsPerSecond;
+  cube.rotation.y += delta * cubeSpeedInUnitsPerSecond;
 
-  cube.rotation.x += delta * speedInUnitsPerSecond;
-  cube.rotation.y += delta * speedInUnitsPerSecond;
+  // Tick counters.
+  oneTick += delta;
+  twoTicks += delta;
+  sixTicks += delta;
 
-  console.log(delta, delta * speedInUnitsPerSecond);
+  // Events happening every one tick.
+  if (oneTick > 0.5) {
+    callApi('GET', '/');
+    oneTick = 0; // Reset tick counter.
+  }
 
-  // Render the scene
+  // Events every two ticks.
+  if (twoTicks > 2) {
+    callApi('PUT', '/ticks');
+    twoTicks = 0;
+  }
+
+  // Events every six ticks.
+  if (sixTicks > 6) {
+    callApi('PUT', '/fishes');
+    sixTicks = 0;
+  }
+
+  // Render the scene.
   renderer.render(scene, camera);
-
   requestAnimationFrame(render);
 };
 
