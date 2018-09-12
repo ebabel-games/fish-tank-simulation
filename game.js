89ce65dfdd627066922cad9171e9bf68075fa631
Thomas Amar <hello@ebabel.eu>
2018-09-12 16:44:27 +0200

    bridge api response with UI

--- game.js
+++ game.js
@@ -5,6 +5,9 @@ const domain = 'https://fish-tank-simulation-ivxfzwdyav.now.sh';
 
 let latestLog;
 
+// Collection of all fishes, updated by API calls.
+let fishes = [];
+
 const updateLogs = (logs) => {
   const logsWindow = document.getElementById('logsWindow');
 
@@ -27,21 +30,44 @@ const updateLogs = (logs) => {
   logsWindow.scrollTop = logsWindow.scrollHeight;
 }
 
-const callApi = (method = 'GET', endpoint) => {
+const callApi = (method = 'GET', endpoint, init = false) => {
   const url = `${domain}${endpoint}`;
   const xhr = new XMLHttpRequest();
   xhr.open(method, url, true);
   xhr.onreadystatechange = () => {
+    let response;
     if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 201)) {
-      const response = JSON.parse(xhr.response);
+      response = JSON.parse(xhr.response);
       updateLogs(response.logs);
+    }
+
+    if (xhr.readyState === 4 && xhr.status === 201 && endpoint === '/ticks') {
+      fishes = response.ticks[0].fishes;
+    }
+
+    if (xhr.readyState === 4 && xhr.status === 201 && endpoint === '/ticks' && init) {
+      fishes.map(fish => {
+        const sprite = createFishSprite(fish);
+        particleGroup.add(sprite);
+        particleAttributes.startPosition.push(sprite.position.clone());
+        particleAttributes.randomness.push(Math.random());
+        scene.add(particleGroup);
+      });
+    }
+
+    if (xhr.readyState === 4 && xhr.status === 201 && endpoint === '/fishes') {
+      const sprite = createFishSprite(response.fish);
+      particleGroup.add(sprite);
+      particleAttributes.startPosition.push(sprite.position.clone());
+      particleAttributes.randomness.push(Math.random());
+      scene.add(particleGroup);
     }
   };
   xhr.send();
 };
 
-// First setup: start by creating a fish.
-callApi('PUT', '/fishes');
+// First call: initialize and create multiple fishes, if any.
+callApi('PUT', '/ticks', true);
 
 // THREE.js setup.
 const clock = new THREE.Clock();
@@ -68,7 +94,7 @@ floorTexture.repeat.set(100, 100);
 const floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, side: THREE.DoubleSide });
 const floorGeometry = new THREE.PlaneGeometry(10000, 10000, 100, 100);
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.position.y = -1;
+floor.position.y = -50;
 floor.rotation.x = Math.PI / 2;
 scene.add(floor);
 
@@ -79,68 +105,74 @@ const skyBoxMaterial = new THREE.MeshBasicMaterial({ color: 0x3c6478, side: THRE
 const skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
 scene.add(skyBox);
 
-// Underwater fog.
+// Underwater "fog".
 const fog = new THREE.FogExp2(0x3c6478, 0.0005);
 scene.fog = fog;
+
+// Create a fish sprite and add it to the collection of fishSprites.
+const createFishSprite = (fish) => {
+  const spriteMaterial = new THREE.SpriteMaterial({ map: particleTexture, color: 0xffffff });
+  const sprite = new THREE.Sprite(spriteMaterial);
+
+  sprite.name = fish.name;
+  sprite.visible = false;
+  sprite.scale.set(fish.life, fish.life, 1.0);
+  sprite.position.set(fish.location[0], fish.location[1], fish.location[1]);
+  sprite.position.setLength(radiusRange * (Math.random() * 0.1 + 0.9));
+  sprite.material.color.setHSL(Math.random(), 0.9, 0.7);
+  sprite.material.blending = THREE.AdditiveBlending;
+
+  return sprite;
+};
 
 // Particles setup.
 const particleTexture = new THREE.TextureLoader().load('assets/spark.png');
 const particleGroup = new THREE.Object3D();
 particleGroup.position.y = 55;
 const particleAttributes = { startSize: [], startPosition: [], randomness: [] };
-let totalParticles = 18;
 const radiusRange = 50;
-
-for (let i = 0; i < totalParticles; i++) {
-  const spriteMaterial = new THREE.SpriteMaterial({ map: particleTexture, color: 0xffffff });
-  const sprite = new THREE.Sprite(spriteMaterial);
-  sprite.scale.set( 32, 32, 1.0 ); // imageWidth, imageHeight
-  sprite.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
-  sprite.position.setLength(radiusRange * (Math.random() * 0.1 + 0.9));
-  sprite.material.color.setHSL(Math.random(), 0.9, 0.7);
-  sprite.material.blending = THREE.AdditiveBlending; // Glowing particles.
-  particleGroup.add(sprite);
-
-  // Add variable qualities to arrays, if they need to be accessed later.
-  particleAttributes.startPosition.push(sprite.position.clone());
-  particleAttributes.randomness.push(Math.random());
-}
-scene.add(particleGroup);
 
 const animate = () => {
   const time = 4 * clock.getElapsedTime();
   
   for (let c = 0; c < particleGroup.children.length; c++) {
     const sprite = particleGroup.children[c];
-    
-    // Pulse away and towards center. Individual rates of movement.
     const a = particleAttributes.randomness[c] + 1;
     const pulseFactor = Math.sin(a * time) * 0.1 + 0.9;
-    sprite.position.x = particleAttributes.startPosition[c].x * pulseFactor;
-    sprite.position.y = particleAttributes.startPosition[c].y * pulseFactor;
-    sprite.position.z = particleAttributes.startPosition[c].z * pulseFactor;	
-  }
 
-  // Rotate the entire group
-  particleGroup.rotation.y = time * 0.75;
+    // Get the latest state of each fish represented by a sprite.
+    const fish = fishes.filter(f => f.name === sprite.name);
+
+    if (fish && fish.length > 0) {
+      sprite.position.set(
+        fish[0].location[0] * pulseFactor,
+        fish[0].location[1] * pulseFactor,
+        fish[0].location[2] * pulseFactor
+      )
+      sprite.scale.set(fish[0].life, fish[0].life, 1.0);
+      sprite.visible = true;
+    } else {
+      sprite.visible = false;
+    }
+  }
   
   controls.update();
 };
 
 // Render Loop.
-let oneTick = 0;
+let quarterTick = 0;
 let tenTicks = 0;
 const render = () => {
   const delta = clock.getDelta(); // Calculate Delta.
 
   // Tick counters.
-  oneTick += delta;
+  quarterTick += delta;
   tenTicks += delta;
 
   // Events happening every one tick.
-  if (oneTick > 1) {
+  if (quarterTick > 0.25) {
     callApi('PUT', '/ticks');
-    oneTick = 0; // Reset tick counter.
+    quarterTick = 0; // Reset tick counter.
   }
 
   // Events every ten ticks.
