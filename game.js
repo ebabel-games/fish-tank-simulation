41f27cc7fa143035a0498f6d3f095c25be2a0c03
Thomas Amar <hello@ebabel.eu>
2018-09-12 08:02:59 +0200

    remove the test cube. add a floor and orbit controls. remove old log messages from the front-end, to save performance.

--- game.js
+++ game.js
@@ -12,6 +12,14 @@ const updateLogs = (logs) => {
     logsWindow.appendChild(p);
   });
 
+  // Not to damage client-side performance, older messages are removed,
+  // otherwise the number of elements keeps growing other time.
+  if (logsWindow.childNodes.length > 7) {
+    while (logsWindow.childNodes.length > 7) {
+      logsWindow.removeChild(logsWindow.firstChild);
+    }
+  }
+
   // Scroll to the bottom of logsWindow.
   logsWindow.scrollTop = logsWindow.scrollHeight;
 }
@@ -28,35 +36,42 @@ const callApi = (method = 'GET', endpoint) => {
   xhr.send();
 };
 
+// First setup: start by creating a fish.
+callApi('PUT', '/fishes');
+
 // THREE.js setup.
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 4;
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 20000);
+camera.position.set(0, 150, 400);
+camera.lookAt(scene.position);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#0e0727");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-// Create a Cube Mesh with basic material.
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Light.
+const light = new THREE.PointLight(0xffffff);
+light.position.set(0,250,0);
+scene.add(light);
 
-// First setup: start by creating a fish.
-callApi('PUT', '/fishes');
+// Floor
+var floorTexture = new THREE.ImageUtils.loadTexture('assets/checkerboard.jpg');
+floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
+floorTexture.repeat.set( 10, 10 );
+var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.position.y = -0.5;
+floor.rotation.x = Math.PI / 2;
+scene.add(floor);
 
 // Render Loop.
 let oneTick = 0;
 let tenTicks = 0;
 const render = () => {
   const delta = clock.getDelta(); // Calculate Delta.
-
-  // Cube animation.
-  const cubeSpeedInUnitsPerSecond = 2;
-  cube.rotation.x += delta * cubeSpeedInUnitsPerSecond;
-  cube.rotation.y += delta * cubeSpeedInUnitsPerSecond;
 
   // Tick counters.
   oneTick += delta;
